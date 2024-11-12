c01a4969610b5fd3b362d2bf8be1e4ed2288ee1e
Ivan <ivan.bethus@gmail.com>
2024-11-12 20:56:01 +0100

    Init the project

---
+++ src/main/java/com/github/thecyclistdiary/MyAction.java
@@ -0,0 +1,14 @@
+package com.github.thecyclistdiary;
+
+import io.quarkiverse.githubaction.Action;
+import io.quarkiverse.githubaction.Commands;
+
+public class MyAction {
+
+    @Action
+    void action(Commands commands) {
+        commands.notice("Hello from Quarkus GitHub Action");
+
+        commands.appendJobSummary(":wave: Hello from Quarkus GitHub Action");
+    }
+}
