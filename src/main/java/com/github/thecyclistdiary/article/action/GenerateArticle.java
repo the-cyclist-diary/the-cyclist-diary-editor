e6bbc1e9948fbf02e358d1d057e230a1535a4953
Ivan <ivan.bethus@gmail.com>
2025-06-30 15:34:31 +0200

    refactor: reorganize imports and improve error logging in GenerateArticle class

--- src/main/java/com/github/thecyclistdiary/article/action/GenerateArticle.java
+++ src/main/java/com/github/thecyclistdiary/article/action/GenerateArticle.java
@@ -1,22 +1,5 @@
 package com.github.thecyclistdiary.article.action;
 
-import com.github.thecyclistdiary.NoLabelFoundException;
-import com.github.thecyclistdiary.article.dto.Article;
-import com.github.thecyclistdiary.article.dto.ArticleGallery;
-import io.quarkiverse.githubaction.Action;
-import io.quarkiverse.githubaction.Commands;
-import io.quarkiverse.githubaction.Context;
-import io.quarkiverse.githubaction.Inputs;
-import io.quarkiverse.githubapp.event.Issue;
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.kohsuke.github.GHEventPayload;
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueStateReason;
-import org.kohsuke.github.GHRepository;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -26,8 +9,27 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.stream.Stream;
+
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.kohsuke.github.GHEventPayload;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueStateReason;
+import org.kohsuke.github.GHRepository;
+
+import com.github.thecyclistdiary.NoLabelFoundException;
+import com.github.thecyclistdiary.article.dto.Article;
+import com.github.thecyclistdiary.article.dto.ArticleGallery;
+
+import io.quarkiverse.githubaction.Action;
+import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubaction.Context;
+import io.quarkiverse.githubaction.Inputs;
+import io.quarkiverse.githubapp.event.Issue;
+import io.quarkus.logging.Log;
 
 public class GenerateArticle {
 
@@ -36,7 +38,7 @@ public class GenerateArticle {
 
     @Action("Generate article")
     void action(Commands commands, Context context, Inputs inputs, @Issue GHEventPayload.Issue issuePayload,
-                GHRepository repository) throws IOException, GitAPIException {
+            GHRepository repository) throws IOException, GitAPIException {
         commands.echo("Traitement de l'issue...");
         GHIssue issue = issuePayload.getIssue();
         if (ARTICLE.equals(issue.getMilestone().getTitle())) {
@@ -56,7 +58,8 @@ public class GenerateArticle {
                 Path adventureFolder = repoDirectory.resolve("content").resolve("adventures");
                 commands.echo(String.format("Recherche de l'aventure %s", adventureName));
                 try (Stream<Path> files = Files.list(adventureFolder)) {
-                    Optional<Path> matchingAdventure = files.filter(path -> adventureName.equalsIgnoreCase(path.getFileName().toString()))
+                    Optional<Path> matchingAdventure = files
+                            .filter(path -> adventureName.equalsIgnoreCase(path.getFileName().toString()))
                             .findAny();
                     if (matchingAdventure.isPresent()) {
                         writeArticle(commands, matchingAdventure.get(), adventureFolder, article, git, username,
@@ -67,13 +70,14 @@ public class GenerateArticle {
                     }
                 }
             } catch (NoLabelFoundException e) {
-                logErrorAndExit(commands, "L'issue n'a pas de label !");
+                logErrorAndExit(commands, "L'issue n'a pas de label !", e);
             }
         }
         commands.appendJobSummary(String.format(":wave: L'article %s a bien été généré !", issue.getTitle()));
     }
 
-    private static void writeArticle(Commands commands, Path adventurePath, Path adventureFolder, Article article, Git git, String username, String token, GHIssue issue, GHRepository repository) throws IOException {
+    private static void writeArticle(Commands commands, Path adventurePath, Path adventureFolder, Article article,
+            Git git, String username, String token, GHIssue issue, GHRepository repository) throws IOException {
         Path articlePath = adventureFolder.resolve(adventurePath).resolve(article.title());
         Files.createDirectory(articlePath);
         parseArticle(commands, article, articlePath);
@@ -81,9 +85,9 @@ public class GenerateArticle {
             commitAndPush(commands, git, article, username, token, issue, articlePath, repository);
             issue.close(GHIssueStateReason.COMPLETED);
         } catch (GitAPIException e) {
-            logErrorAndExit(commands, "Le nouvel article n'a pas pu être commité");
+            logErrorAndExit(commands, "Le nouvel article n'a pas pu être commité", e);
         } catch (IOException e) {
-            logErrorAndExit(commands, "Impossible de mettre à jour l'issue");
+            logErrorAndExit(commands, "Impossible de mettre à jour l'issue", e);
         }
     }
 
@@ -97,10 +101,10 @@ public class GenerateArticle {
             }
         } catch (IOException e) {
             logErrorAndExit(commands,
-                    "Le nouvel article n'a pas pu être écrit à cause d'une erreur d'I/O");
+                    "Le nouvel article n'a pas pu être écrit à cause d'une erreur d'I/O", e);
         } catch (URISyntaxException e) {
             logErrorAndExit(commands,
-                    "Impossible de télécharger le fichier gpx lié à l'article");
+                    "Impossible de télécharger le fichier gpx lié à l'article", e);
         }
     }
 
@@ -115,13 +119,16 @@ public class GenerateArticle {
                                 StandardCopyOption.REPLACE_EXISTING);
                         commands.echo(String.format("L'image %s a bien été téléchargé", image.name()));
                     } catch (IOException | URISyntaxException e) {
-                        logErrorAndExit(commands, String.format("Impossible de télécharger l'image à l'adresse %s", image.url()));
+                        logErrorAndExit(commands,
+                                String.format("Impossible de télécharger l'image à l'adresse %s", image.url()),
+                                e);
 
                     }
                 });
     }
 
-    private static void downloadGpxFile(Commands commands, Article article, Path articlePath) throws IOException, URISyntaxException {
+    private static void downloadGpxFile(Commands commands, Article article, Path articlePath)
+            throws IOException, URISyntaxException {
         InputStream in = new URI(article.gpxUrl()).toURL().openStream();
         String gpxName = String.format("%s.gpx", article.title());
         Files.copy(in, articlePath.resolve(gpxName),
@@ -129,13 +136,15 @@ public class GenerateArticle {
         commands.echo(String.format("Le fichier %s a bien été téléchargé", gpxName));
     }
 
-    private static void logErrorAndExit(Commands commands, String message) {
+    private static void logErrorAndExit(Commands commands, String message, Exception e) {
         commands.error(message);
+        Log.error(e.getMessage(), e);
         System.exit(ERROR_STATUS);
     }
 
-    private static void commitAndPush(Commands commands, Git git, Article article, String username, String token, GHIssue issue,
-                                      Path articlePath, GHRepository repository) throws GitAPIException, IOException {
+    private static void commitAndPush(Commands commands, Git git, Article article, String username, String token,
+            GHIssue issue,
+            Path articlePath, GHRepository repository) throws GitAPIException, IOException {
         String newBranch = article.title().replaceAll(" ", "_");
         git.branchCreate()
                 .setName(newBranch)
@@ -161,8 +170,7 @@ public class GenerateArticle {
                 String.format("article/%s", article.title()),
                 article.title(),
                 "main",
-                commitMessage
-        );
+                commitMessage);
         issue.comment(String.format("Article généré le %s dans content/adventures/%s.md", LocalDate.now(),
                 articlePath));
     }
